aec47eff1641fed9ede297b276f2a1a24195e7a2
Eduardo Marçal Oliveira <edumarcalnet@gmail.com>
2022-08-06 14:37:47 -0300

    :arrow_up: update dependencies

--- app/src/main/java/me/dio/businesscard/data/BusinessCardRepository.kt
+++ app/src/main/java/me/dio/businesscard/data/BusinessCardRepository.kt
@@ -4,7 +4,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class BusinessCardRepository(private val dao: IBusinessCardDao){
+class BusinessCardRepository(private val dao: IBusinessCardDao) {
+
     fun insert(businessCard: BusinessCard) = runBlocking {
         launch(Dispatchers.IO) {
             dao.insert(businessCard)
