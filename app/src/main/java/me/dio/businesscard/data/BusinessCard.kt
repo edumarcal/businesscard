34afc6ff99af214d82fdb6e860cd030024dfe904
Eduardo Marçal Oliveira <edumarcalnet@gmail.com>
2022-08-06 11:15:24 -0300

    :sparkles: setup database

---
+++ app/src/main/java/me/dio/businesscard/data/BusinessCard.kt
@@ -0,0 +1,14 @@
+package me.dio.businesscard.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class BusinessCard(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val company: String,
+    val phone: String,
+    val email: String,
+    val background: String
+)
---
+++ app/src/main/java/me/dio/businesscard/data/BusinessCardRepository.kt
@@ -0,0 +1,15 @@
+package me.dio.businesscard.data
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class BusinessCardRepository(private val dao: IBusinessCardDao){
+    fun insert(businessCard: BusinessCard) = runBlocking {
+        launch(Dispatchers.IO) {
+            dao.insert(businessCard)
+        }
+    }
+
+    fun getAll() = dao.getAll();
+}
