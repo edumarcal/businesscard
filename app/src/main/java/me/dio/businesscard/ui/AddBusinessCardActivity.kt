d4d9bb0482f77e01173f13cf795e44595e64f249
Eduardo Marçal Oliveira <edumarcalnet@gmail.com>
2022-08-06 00:56:29 -0300

    :sparkles: add AddBusinessCardActivity

---
+++ app/src/main/java/me/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,14 @@
+package me.dio.businesscard.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.dio.businesscard.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import me.dio.businesscard.databinding.ActivityMainBinding
@@ -10,5 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
